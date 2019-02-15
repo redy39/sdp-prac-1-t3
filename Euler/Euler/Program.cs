3460a67970b9b7f1fe04ce6e29d4aa597ccb47a3
nightstreak <prumo101@gmail.com>
2019-02-15 16:55:23 +0200

    Promblem 1 from Thapelo Marumo

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -10,6 +10,18 @@ namespace Euler
     {
         static void Main(string[] args)
         {
+            //Thapelo Marumo Problem 1
+            int sum = 0;
+            int i = 0;
+            while ( i < 1000)
+            {
+                if (i % 3 == 0 || i % 5 == 0  )
+                {
+                    sum += i;
+                }
+                i++;
+            }
+            Console.WriteLine(Convert.ToString(sum));
         }
     }
 }
