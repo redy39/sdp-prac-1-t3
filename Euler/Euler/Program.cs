e63b6b908d153a8218564fee476dc8715fc7f4c4
Shumbaneverdies <31037140+Shumbaneverdies@users.noreply.github.com>
2019-02-16 21:44:54 +0200

    Tapiwa's added code (problem number 2)

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -35,6 +35,26 @@ namespace Euler
                 count++;
             }
             Console.WriteLine(Convert.ToString(sum));
+
+
+            //Tapiwa Pawandiwa Problem 2
+            int Fibnum = 0;
+            int prev_one = 1;  //we do this because the first numbers in the fibonnaci sequence are 0,1,1,2,3...
+            int prev_two= 1;
+            int result = 0;
+
+            while (Fibnum < 4000000)
+            {
+                if (Fibnum % 2 ==0) // this tells us whether the number is even or odd, in this case we looking for only even numbers 
+                {
+                    result = result + Fibnum;
+                }
+                Fibnum = prev_one + prev_two; // adding the two previous numbers , we calculate the next number in the fibonnaci sequence by adding the two previous numbers 
+                prev_two = prev_one; // if our first number is 0 , we use force the code to go to 2 so we start the sequence, so if 2 is the next number , what is the previous number ? 1 
+                prev_one = Fibnum;  // our second previous number is the number we are dealing with , we do this in order to calculate the next number 
+            }
+            Console.WriteLine(Convert.ToString(result));
+
         }
     }
 }
